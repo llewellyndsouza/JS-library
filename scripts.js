16d0242083721b3e09168316016be2fa42b7277e
llewellyndsouza <lledsouza2209@gmail.com>
2021-02-08 18:50:34 +0530

    Overlay to add book to library

--- scripts.js
+++ scripts.js
@@ -15,12 +15,27 @@ function printAllBooks() {
   });
 }
 
-let newBookButton = document.getElementById('newBook');
-    newBookButton.addEventListener('click',()=>{
-        displayNewbookForm();
-    });
+let newBookButton = document.getElementById("newBook");
+newBookButton.addEventListener("click", () => {
+  displayNewbookForm();
+});
 
 function displayNewbookForm() {
-    document.window
-    addBookToLibrary("testbook",69);
+    let overlay = document.getElementById('form-overlay');
+    overlay.style.display = 'grid';
+}
+
+function addBookButton() {
+  console.log("hits here");
+  let bookName = document.getElementById("bookname");
+  let pages = document.getElementById("pages");
+
+  addBookToLibrary(bookName.value, pages.value);
+
+  bookName.value = '';
+  pages.value = '';
+
+  // Hides the overlay/book add form
+  let overlay = document.getElementById('form-overlay');
+    overlay.style.display = 'none';
 }
